4544d9ee412d5cc7b95540ce3c24b54b901352a2
gitSaulius <saule777@gmail.com>
2023-06-20 22:17:15 +0300

    for ciklo uzduotys

---
+++ for_ciklas.py
@@ -0,0 +1,26 @@
+# Parašykite programa, kuri spausdina skaičius nuo 10 iki 1 mažėjančia tvarka.
+
+# for skaicius in range (10, 0, -1):
+#     print(skaicius, end=" ")
+
+
+# Parašykite programa, kuri spausdina kiekvieną raidę žodyje.
+
+# zodis = "uzduotis"
+# sarasas = []
+# for raide in zodis:
+#     sarasas.append(raide)
+# print(sarasas)
+
+
+#Parašykite programą,kuri priima vartotojo įvestą sąrašą skaičių ir grąžina tik tuos skaičius,kurie yra unikalūs sąraše.
+# Pavyzdžiui, jei vartotojas įveda sąrašą [1, 2, 3, 2, 4, 5, 3], programa turėtų grąžinti [1, 4, 5], nes šie skaičiai
+# yra unikalūs sąraše.
+
+# skaiciu_sarasas = []
+# skaiciu_sarasas = [int(skaicius) for skaicius in input("Iveskite skaiciu sarasa: ").split()]
+# naujas_sarasas = [skaicius for skaicius in skaiciu_sarasas if skaiciu_sarasas.count(skaicius) == 1]
+# print(naujas_sarasas)
+
+
+
